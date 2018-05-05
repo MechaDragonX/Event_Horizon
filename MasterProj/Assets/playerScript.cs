43bec7ac4fe4bfce7068486eba7710a8c5cced31
yifan <yifan@YIFEIXU-PC>
2018-05-04 22:37:06 -0700

    Project created (With bad horizontal movements)

---
+++ MasterProj/Assets/playerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour {
+
+    public float speed = 500f;
+    public float jumpStrength = 1000f;
+    public bool canJump = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame`
+	void Update () {
+        Vector2 dir = new Vector2();
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            dir.x -= speed;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            dir.x += speed;
+        }
+
+        if (canJump)
+        {
+            dir.y += jumpStrength;
+            canJump = false;
+        }
+
+        this.GetComponent<Rigidbody2D>().AddForce(dir);
+    }
+}
