ba42a5a6cb1af2689b9418396abfd788b00c68bd
yifan <yifan@LAPTOP-D149ADF3>
2018-05-05 19:39:12 -0700

    Player movement + Camera

---
+++ MasterProj/Assets/cameraScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour {
+
+    public GameObject focus;
+    private float zPos;
+    public float lerpFactor = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+        zPos = this.transform.position.z;
+        //Vector3 target = focus.transform.position;
+        //target.z = zPos;
+        //this.transform.position = target;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 target = focus.transform.position;
+        target.z = zPos;
+        this.transform.position = Vector3.Lerp(this.transform.position, target, lerpFactor);
+	}
+}
--- MasterProj/Assets/playerScript.cs
+++ MasterProj/Assets/playerScript.cs
@@ -4,35 +4,46 @@ using UnityEngine;
 
 public class playerScript : MonoBehaviour {
 
-    public float speed = 500f;
-    public float jumpStrength = 1000f;
-    public bool canJump = true;
+    public float topSpeed = 1f;
+    public float accelerationFactor = 0.6f;
+    public float jumpStrength = 1f;
+    private Rigidbody2D rigid;
 
 	// Use this for initialization
 	void Start () {
-
-        canJump = true;
+        rigid = this.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame`
-	void Update () {
-        Vector2 dir = new Vector2();
-        if(Input.GetKeyDown(KeyCode.A))
+	void FixedUpdate () {
+        //Stop the player from spinning
+        rigid.angularVelocity = 0f;
+        this.transform.eulerAngles = new Vector3();
+
+        float dT = Time.deltaTime;
+
+        //Controls
+        //The way this is designed to work is that it is fast for the player to turn around.
+        Vector2 currentVector = rigid.velocity;
+        float intendedVelocity = 0f;
+
+        if(Input.GetKey(KeyCode.A))
         {
-            dir.x -= speed;
+            intendedVelocity -= topSpeed;
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
-            dir.x += speed;
+            intendedVelocity += topSpeed;
         }
+        currentVector.x += accelerationFactor * (intendedVelocity - currentVector.x);
+        rigid.velocity = currentVector;
+    }
 
-        //if(canJump)
-        //{
-
-        //    dir.y += jumpStrength;
-        //    canJump = false;
-        //}
-
-        this.GetComponent<Rigidbody2D>().AddForce(dir);
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.AddForce(new Vector2(0,jumpStrength));
+        }
     }
 }
